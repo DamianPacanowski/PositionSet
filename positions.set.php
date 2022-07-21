1ffd759a57ff4eeb489fd60868c55156556f0504
DamianPacanowski <60273697+DamianPacanowski@users.noreply.github.com>
2022-07-21 20:54:23 +0200

    Update positions.set.php

--- positions.set.php
+++ positions.set.php
@@ -1,90 +1,90 @@
 <?php
-class positions
-{
-	private $string;
-	private $type;
-	private $action;
-	private $net;
-	private $position;
-	private $output;
-	public function catch($string, $type, $action, $net) 
+	class positions
 	{
-		$this->string = $string;
-		$this->type = $type;
-		$this->action = $action;
-		$this->net = $net;
-		$this->position = NULL;
-		if(self::CatchPositions()) 
+		private $string;
+		private $type;
+		private $action;
+		private $net;
+		private $position;
+		private $output;
+		public function catch($string, $type, $action, $net) 
 		{
-			$output = self::CatchPositions();
-			$this->output = $output;
-			return $this->output;
-		} 
-		else 
-		{
-            return false; 
-        }
-		
-		
-    }
-	private function CatchPositions() 
-	{
-		if( $this->action == 1 ) 
-		{
-			$FBIT=NULL;
-			$POSITION=NULL;
-			$STRLEN=STRLEN($this->string);
-			FOREACH(STR_SPLIT($this->string) as $BIT)
+			$this->string = $string;
+			$this->type = $type;
+			$this->action = $action;
+			$this->net = $net;
+			$this->position = NULL;
+			if(self::CatchPositions()) 
 			{
-				FOREACH(STR_SPLIT($this->net) as $_NO => $_BIT)
+				$output = self::CatchPositions();
+				$this->output = $output;
+				return $this->output;
+			} 
+			else 
+			{
+		    return false; 
+		}
+
+
+	    }
+		private function CatchPositions() 
+		{
+			if( $this->action == 1 ) 
+			{
+				$FBIT=NULL;
+				$POSITION=NULL;
+				$STRLEN=STRLEN($this->string);
+				FOREACH(STR_SPLIT($this->string) as $BIT)
 				{
-					IF($_BIT==$BIT)
+					FOREACH(STR_SPLIT($this->net) as $_NO => $_BIT)
 					{
-						$POSITION.=$_NO;
-						$FBIT.=strlen($_NO);
+						IF($_BIT==$BIT)
+						{
+							$POSITION.=$_NO;
+							$FBIT.=strlen($_NO);
+						}
 					}
 				}
+				$this->position = $POSITION;
+				$this->fbit = $FBIT;
+
+				if($this->type == 0)
+				{
+					$output=$this->position.'000'.$this->fbit;
+				}
+				elseif($this->type == 1)
+				{
+					$output=$this->position;
+				}
+				elseif($this->type == 2)
+				{
+					$output=$this->fbit;
+				}
 			}
-			$this->position = $POSITION;
-			$this->fbit = $FBIT;
-			
-			if($this->type == 0)
+			elseif( $this->action == 0 )
 			{
-				$output=$this->position.'000'.$this->fbit;
+				$_SIEC =$this->net;
+
+				if($this->type == 0)
+				{
+					$_Ex=EXPLODE('000',$this->string);
+					$_liczby=$_Ex[0];
+					$_strlen_liczb=$_Ex[1];
+				}
+				$_CHARACTERS=NULL;
+				$noxe=null;
+				$_liczbyxv=null;
+				$_splxx1=str_split($_strlen_liczb);
+				foreach($_splxx1 as $noxs)
+				{
+					$noxe+=$noxs;
+					$noxy=$noxe-$noxs;
+					$_liczbyxv= substr($_liczby, $noxy, $noxs);
+					$_CHARACTERS.= $_SIEC[intval($_liczbyxv)];
+				}
+				$output= $_CHARACTERS;
 			}
-			elseif($this->type == 1)
-			{
-				$output=$this->position;
-			}
-			elseif($this->type == 2)
-			{
-				$output=$this->fbit;
-			}
+			return $output;
 		}
-		elseif( $this->action == 0 )
-		{
-			$_SIEC =$this->net;
-			
-			if($this->type == 0)
-			{
-				$_Ex=EXPLODE('000',$this->string);
-				$_liczby=$_Ex[0];
-				$_strlen_liczb=$_Ex[1];
-			}
-			$_CHARACTERS=NULL;
-			$noxe=null;
-			$_liczbyxv=null;
-			$_splxx1=str_split($_strlen_liczb);
-			foreach($_splxx1 as $noxs)
-			{
-				$noxe+=$noxs;
-				$noxy=$noxe-$noxs;
-				$_liczbyxv= substr($_liczby, $noxy, $noxs);
-				$_CHARACTERS.= $_SIEC[intval($_liczbyxv)];
-			}
-			$output= $_CHARACTERS;
-		}
-		return $output;
 	}
-}
 ?>
